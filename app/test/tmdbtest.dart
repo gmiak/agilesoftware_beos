f038b8257705649e8687901a33d43290ec5f8a5c
gmiak <kayss.g@gmail.com>
2021-04-19 15:22:58 +0200

    Created class movieModelInfo and movieViewInfo

--- app/test/tmdbtest.dart
+++ app/test/tmdbtest.dart
@@ -1,26 +1,26 @@
 import 'package:tmdb_api/tmdb_api.dart';
 
 main() async {
-
 //Nycklar för att göra anrop till databasen.
   var apiKey = 'b4cc0c5b697aa656e542ee4110939d7e';
-  var apiReadAccessTokenV4 = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGNjMGM1YjY5N2FhNjU2ZTU0MmVlNDExMDkzOWQ3ZSIsInN1YiI6IjYwNzg3NGRkMWIxZjNjMDA1N2I4OGYzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qN2UHE8GkoB0tCf0X2xGIkJFeXgMkhx8W2-lBv4Cotk';
-  
+  var apiReadAccessTokenV4 =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGNjMGM1YjY5N2FhNjU2ZTU0MmVlNDExMDkzOWQ3ZSIsInN1YiI6IjYwNzg3NGRkMWIxZjNjMDA1N2I4OGYzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qN2UHE8GkoB0tCf0X2xGIkJFeXgMkhx8W2-lBv4Cotk';
 
-//Nedan följer 3 olika sätt att anropa databasen. Gissar att vi kommer använda den översta utan logg mest. 
+//Nedan följer 3 olika sätt att anropa databasen. Gissar att vi kommer använda den översta utan logg mest.
 //Då borde vi kunna läsa i api:t vad som finns och skriva: await tmdb.v3.<<kategori>>.<<funktion>>();
 //https://developers.themoviedb.org/3/
 
   //api with out console logs
   TMDB tmdb = TMDB(ApiKeys(apiKey, apiReadAccessTokenV4));
-  print(await tmdb.v3.movies.getPouplar()); 
+  //print(await tmdb.v3.movies.getPouplar());
+  print(tmdb.v3.networks);
 
   //api with showing all console logs
   TMDB tmdbWithLogs = TMDB(
     ApiKeys(apiKey, apiReadAccessTokenV4),
     logConfig: ConfigLogger.showAll(),
   );
-  print(await tmdbWithLogs.v3.movies.getPouplar());
+  //print(await tmdbWithLogs.v3.movies.getPouplar());
 
   //api with showing all console logs
   TMDB tmdbWithCustomLogs = TMDB(
@@ -31,5 +31,5 @@ main() async {
       showErrorLogs: true,
     ),
   );
-  print(await tmdbWithCustomLogs.v3.movies.getPouplar());
+  //print(await tmdbWithCustomLogs.v3.movies.getPouplar());
 }
