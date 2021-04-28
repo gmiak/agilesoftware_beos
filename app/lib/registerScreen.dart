10429642dd59d1307bec95a035bbbf7bbe57b638
Abbelonius <albin.rossle@hotmail.com>
2021-04-28 15:28:41 +0200

    Fungerande registrering.

--- app/lib/registerScreen.dart
+++ app/lib/registerScreen.dart
@@ -1,18 +1,17 @@
-import 'package:app/registerScreen.dart';
 import 'package:flutter/material.dart';
 import 'main.dart';
 import 'package:app/networking/authentication.dart';
 
 /*
-Loginskärm som skall använda sig av autentication när man startar appen. 
+Registreringsskärm som skall använda sig av autentication när man startar appen. 
 */
 
-class LoginScreen extends StatefulWidget {
+class RegisterScreen extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _RegisterScreenState createState() => _RegisterScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _RegisterScreenState extends State<RegisterScreen> {
 
   Authentication auth = Authentication();
     TextEditingController emailController = new TextEditingController();
@@ -23,7 +22,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Login Page"),
+        title: Text("Register account"),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -60,15 +59,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     hintText: 'Enter secure password'),
               ),
             ),
-            TextButton(
-              onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
             Container(
               height: 50,
               width: 250,
@@ -76,30 +66,18 @@ class _LoginScreenState extends State<LoginScreen> {
                   color: Colors.blue, borderRadius: BorderRadius.circular(20)),
               child: TextButton(
                 onPressed: () async {
+                  await auth.signUp(emailController.text, passwordController.text);
                   await auth.signIn(emailController.text, passwordController.text);
                   await auth.checkAuth();
                   Navigator.push(
                       context, MaterialPageRoute(builder: (_) => MyHomePage())); //TODO Åberopa authentication för login.
                 },
                 child: Text(
-                  'Login',
+                  'Register',
                   style: TextStyle(color: Colors.white, fontSize: 25),
                 ),
               ),
             ),
-            SizedBox(
-              height: 130,
-            ),
-            TextButton(
-              onPressed: (){
-                Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => RegisterScreen()));
-              },
-              child: Text(
-                'New User? Create Account',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            )
           ],
         ),
       ),
