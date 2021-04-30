c97905d768a7c7a2a5ed72a5c42c6aa67b642726
Abbelonius <albin.rossle@hotmail.com>
2021-04-30 09:19:34 +0200

    Name fix

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -40,20 +40,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // This class is the configuration for the state. It holds the values (in this
 // case the title) provided by the parent (in this case the App widget) and
 // used by the build method of the State. Fields in a Widget subclass are
--- app/lib/resetPasswordScreen.dart
+++ app/lib/resetPasswordScreen.dart
@@ -1,4 +1,4 @@
-import 'package:app/loginscreen.dart';
+import 'package:app/loginScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:app/networking/authentication.dart';
 
