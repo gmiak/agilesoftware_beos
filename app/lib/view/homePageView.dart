beacb88bc5dc2c00f8b94c7ad795e5e5fc8a733f
Abbelonius <albin.rossle@hotmail.com>
2021-05-07 09:22:46 +0200

    Förbered huvudsidan.

--- app/lib/view/homePageView.dart
+++ app/lib/view/homePageView.dart
@@ -1,7 +1,7 @@
-import 'package:app/coList.dart';
+import 'package:app/controller/movieController.dart';
+import 'package:app/model/listModel.dart';
+import 'package:app/view/listViewInfo.dart';
 import 'package:flutter/material.dart';
-import '../movieList.dart';
-import '../swipeMovie.dart';
 
 /*
 ** The App Home Page
@@ -14,60 +14,139 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+
+  int _selectedIndex = 0;
+  List<CommonList> _commonLists = <CommonList>[];
+  TextEditingController listNameController = new TextEditingController();
+  
+    @override
+    void initState() {
+      super.initState();
+      _populateCommonLists();
+    }
+  
+    ///Populates [_commonLists] with lists for this user.
+    void _populateCommonLists() async {
+      final commonLists =
+          await MovieController.getAppRepository().getLists();
+  
+      setState(() {
+        _commonLists = commonLists;
+      });
+    }
+  
+    ///Activates the functionallity of the diffrent choices in the bottom menu.
+    void _onItemTapped(int index) {
+      setState(() {
+        switch (index) {
+          case 0:
+            {
+              openAddListDialog();
+            }
+            break;
+        }
+      });
+    }
+  
+    ///Builds the widget with [ListViewInfo] as body to display the liked movies.
+    ///
+    ///The Widget has a [BottomNavigationBar] with Buttons. 
+  
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text('Home Page'),
+          title: Text('Your lists'),
         ),
-        body: Column(
-          children: <Center>[
-            Center(
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
-              child: ElevatedButton(
-                child: Text("Movies"),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MovieList()),
-                  );
-                },
+        body: ListViewInfo(
+          commonLists: _commonLists,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_circle_outline_rounded,
+                size: 40,
               ),
-              // This trailing comma makes auto-formatting nicer for build methods.
-            ),
-            Center(
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
-              child: ElevatedButton(
-                child: Text("Swipe"),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SwipeMovie(listId: 'testList')),
-                  );
-                },
-              ),
-              // This trailing comma makes auto-formatting nicer for build methods.
-            ),
-            Center(
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
-              child: ElevatedButton(
-                child: Text("Liked"),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => CoList(listId: 'testList')),
-                  );
-                },
-              ),
-              // This trailing comma makes auto-formatting nicer for build methods.
+              label: 'Create list',
             ),
           ],
-        ));
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
+        ),
+      );
+    }
+  
+    ///Opens a Dialog window to be able to add members to a list.
+    Future<String> openAddListDialog() {
+      return showDialog(
+          context: context,
+          builder: (ctx) {
+            return AlertDialog(
+                title: Text('Enter list name.'),
+                actions: <Widget>[
+                  IconButton(
+                    iconSize: 40,
+                    icon: Icon(Icons.arrow_left_sharp),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+                content: SingleChildScrollView(
+                    child: Column(
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: TextField(
+                          controller: listNameController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'List name'),
+                        )),
+                    IconButton(
+                      iconSize: 40,
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        //TODO() Lägg till lista via AppRepository
+                        MovieController.getAppRepository()
+                            .createList(listNameController.text);
+                        showFeedbackDialog(context, 'List created');
+                        listNameController.clear();
+                      },
+                      padding: EdgeInsets.only(top: 20),
+                    )
+                  ],
+                )));
+          });
+    }
+  
+    /// Gives feedback that a user has been added as a member to a list.
+    showFeedbackDialog(BuildContext context, String message) {
+      // set up the button
+      Widget okButton = TextButton(
+        child: Text("OK"),
+        onPressed: () {
+          Navigator.of(context).pop(); // dismiss dialog
+        },
+      );
+  
+      // set up the AlertDialog
+      AlertDialog alert = AlertDialog(
+        title: Text(message),
+        actions: [
+          okButton,
+        ],
+      );
+  
+      // show the dialog
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        },
+      );
+    }
   }
-}
+
